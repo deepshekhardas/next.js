aba054bafa711f23d8d1a1fba5218d5b8ca13318
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-06-18 22:47:33 +0200

    Remove unstable_Postone usage

--- packages/next/src/server/app-render/dynamic-rendering.ts
+++ packages/next/src/server/app-render/dynamic-rendering.ts
@@ -28,7 +28,6 @@ import type {
   ValidationStoreClient,
 } from '../app-render/work-unit-async-storage.external'
 
-// Once postpone is in stable we should switch to importing the postpone export directly
 import React from 'react'
 
 import { DynamicServerError } from '../../client/components/hooks-server-context'
@@ -78,8 +77,6 @@ import {
   allRequiredBoundariesRendered,
 } from './instant-validation/boundary-tracking'
 import type { InstantValidationSampleTracking } from './instant-validation/instant-samples'
-
-const hasPostpone = typeof React.unstable_postpone === 'function'
 
 export type DynamicAccess = {
   /**
@@ -371,38 +368,6 @@ export function abortAndThrowOnSynchronousRequestDataAccess(
   )
 }
 
-/**
- * This component will call `React.postpone` that throws the postponed error.
- */
-type PostponeProps = {
-  reason: string
-  route: string
-}
-export function Postpone({ reason, route }: PostponeProps): never {
-  const dynamicTracking = null
-  postponeWithTracking(route, reason, dynamicTracking)
-}
-
-function postponeWithTracking(
-  route: string,
-  expression: string,
-  dynamicTracking: null | DynamicTrackingState
-): never {
-  assertPostpone()
-  if (dynamicTracking) {
-    dynamicTracking.dynamicAccesses.push({
-      // When we aren't debugging, we don't need to create another error for the
-      // stack trace.
-      stack: dynamicTracking.isDebugDynamicAccesses
-        ? new Error().stack
-        : undefined,
-      expression,
-    })
-  }
-
-  React.unstable_postpone(createPostponeReason(route, expression))
-}
-
 function createPostponeReason(route: string, expression: string) {
   return (
     `Route ${route} needs to bail out of prerendering at this point because it used ${expression}. ` +
@@ -519,18 +484,6 @@ export function formatDynamicAPIAccesses(
     })
 }
 
-function assertPostpone() {
-  if (!hasPostpone) {
-    throw new Error(
-      `Invariant: React.unstable_postpone is not defined. This suggests the wrong version of React was loaded. This is a bug in Next.js`
-    )
-  }
-}
-
-/**
- * This is a bit of a hack to allow us to abort a render using a Postpone instance instead of an Error which changes React's
- * abort semantics slightly.
- */
 export function createRenderInBrowserAbortSignal(): AbortSignal {
   const controller = new AbortController()
   controller.abort(new BailoutToCSRError('Render in Browser'))
